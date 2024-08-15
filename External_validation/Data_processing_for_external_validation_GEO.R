13d58f5c5d7c900e4b7a8a4d547fe6a1aeddf50a
Jeong-Woon, Park <69787143+jeong2624@users.noreply.github.com>
2024-08-15 15:24:17 +0900

    Update and rename Data_processing_for_external_validation.R to Data_processing_for_external_validation_GEO.R

--- External_validation/Data_processing_for_external_validation_GEO.R
+++ External_validation/Data_processing_for_external_validation_GEO.R
@@ -10,7 +10,8 @@ source("Utils.R")
 Gene <- fread("common_gene.csv", data.table = FALSE)$Gene
 
 # Load GSE81538 gene expression data.
-GSE81538_exp = fread(file = "Step4_Validation_independent/Rawdata/GSE81538_gene_expression_405_transformed.csv.gz", data.table = FALSE) %>%
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE81538
+GSE81538_exp = fread(file = "GSE81538_gene_expression_405_transformed.csv.gz", data.table = FALSE) %>%
   column_to_rownames("V1") %>%
   fpkm_to_tpm(Pseudocount = 0.1)
 
@@ -18,7 +19,8 @@ GSE81538_exp <- GSE81538_exp[Gene, ]
 dim(GSE81538_exp) # [1] 813   405
 
 # Load GSE81538 metadata.
-GSE81538_metadata = process_GEO_metadata('Step4_Validation_independent/Rawdata/GSE81538_series_matrix.txt.gz')
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE81538
+GSE81538_metadata = process_GEO_metadata('GSE81538_series_matrix.txt.gz')
 GSE81538_exp = GSE81538_exp[, GSE81538_metadata$title]
 dim(GSE81538_exp) # [1] 813  383
 
@@ -28,6 +30,7 @@ GSE81538_metadata <- GSE81538_metadata[, c("geo_accession", "BRCA_Subtype_PAM50"
 rownames(GSE81538_metadata) <- NULL
 
 # Load GSE135298 gene expression data.
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE135298
 GSE135298_exp = fread(file = "Step4_Validation_independent/Rawdata/GSE135298_OSLO2_EMIT0_RNA-seq.txt.gz", data.table = FALSE) %>%
   column_to_rownames("V1") %>%
   fpkm_to_tpm(Pseudocount = 0.1)
@@ -36,6 +39,7 @@ GSE135298_exp <- GSE135298_exp[Gene, ]
 dim(GSE135298_exp) # [1] 813    93
 
 # Load the GSE135298 metadata.
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE135298
 GSE135298_metadata = process_GEO_metadata('Step4_Validation_independent/Rawdata/GSE135298_series_matrix.txt.gz')
 GSE135298_exp = GSE135298_exp[, GSE135298_metadata$title]
 dim(GSE135298_exp) # [1] 813    82
