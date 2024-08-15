23c13fb0153375ab80c1f1d270fa4f3a353dab78
Jeong-Woon, Park <69787143+jeong2624@users.noreply.github.com>
2024-08-15 12:34:41 +0900

    Update Annotation_preprocess.R

--- Utils/Annotation_preprocess.R
+++ Utils/Annotation_preprocess.R
@@ -1,29 +1,39 @@
-# 작업 디렉토리 불러오기
-setwd("~/OneDrive - inu.ac.kr/SSU_project/논문작성/저널논문_가제본/Code & Result/Step1_Data_collection_preprocessing")
+"
+Created on Thr Aug 15 2024
+
+@author: Jeong-Woon, Park
+
+"
+
 source("Code/Utils.R")
 
-# 데이터 불러오기 및 전처리
-GSE96058 <- fread("Rawdata/GPL11154-GPL18573_gene.csv")$Gene
-GSE81538 <- fread("Rawdata/GSE81538_gene.csv")$Gene
+# Extract gene symbols from the GEO gene expression datasets.
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE96058
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE81538
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE135298
+GSE96058 <- fread("GPL11154-GPL18573_gene.csv")$Gene
+GSE81538 <- fread("GSE81538_gene.csv")$Gene
 GSE135298 <- fread("Rawdata/GSE135298_gene.csv")$Gene
 
-gencode.v22 <- fread("Rawdata/gencode.v22.annotation.gene.probeMap", data.table = FALSE) %>%
+# Gene annotation preprocessing
+# https://gdc-hub.s3.us-east-1.amazonaws.com/download/gencode.v22.annotation.gene.probeMap
+gencode.v22 <- fread("gencode.v22.annotation.gene.probeMap", data.table = FALSE) %>%
   filter(!chrom %in% c("chrX", "chrY", "chrM")) %>%
   mutate(gene_length = chromEnd - chromStart + 1) %>%
   arrange(desc(gene_length)) %>%
   distinct(gene, .keep_all = TRUE) %>%
   arrange(chrom)
 
-# 공통 유전자 찾기
+# Extract common genes.
 common_gene <- Reduce(intersect, list(gencode.v22$gene, GSE135298, GSE81538, GSE96058))
 common_gene <- unique(common_gene)
 
-# 공통 유전자만 선택하고 필요없는 열 제거하기
 gencode.v22 <- gencode.v22 %>%
   filter(gene %in% common_gene)
 
 # Peak annotation preprocessing
-peak <- readPeakFile("Rawdata/brca_promoter.txt")
+# https://api.gdc.cancer.gov/data/d28f95fc-af3b-497d-806b-eb0625d6c831
+peak <- readPeakFile("brca_promoter.txt")
 peak_anno <- annotatePeak(peak, tssRegion = c(-2000, 2000), 
                              TxDb = TxDb.Hsapiens.UCSC.hg38.knownGene,
                              annoDb = "org.Hs.eg.db")
@@ -38,5 +48,5 @@ df_peak_anno <- as.data.frame(peak_anno@anno) %>%
   distinct(SYMBOL,.keep_all = TRUE) %>%
   arrange(seqnames)
 
-fwrite(gencode.v22, "Rawdata/gencode.v22.annotation.csv")
-fwrite(df_peak_anno, "Rawdata/peak.annotation.csv")
+fwrite(gencode.v22, "gencode.v22.annotation.csv")
+fwrite(df_peak_anno, "peak.annotation.csv")
