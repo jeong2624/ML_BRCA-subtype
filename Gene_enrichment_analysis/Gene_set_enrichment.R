cf32920f530555e106a962105abcc16a5c634fb3
Jeong-Woon, Park <69787143+jeong2624@users.noreply.github.com>
2024-08-15 13:03:55 +0900

    Add files via upload

---
+++ Gene_enrichment_analysis/Gene_set_enrichment.R
@@ -0,0 +1,49 @@
+"
+Created on Sat July 20 2024
+
+@author: Jeong-Woon, Park
+"
+
+# Set working directory
+setwd("~/OneDrive - inu.ac.kr/SSU_project/논문작성/저널논문_가제본/Code & Result")
+
+# Load the package related to enrichment analysis.
+packages = c("tidyverse", "data.table", "clusterProfiler", "org.Hs.eg.db", "enrichplot")
+lapply(packages, library, character.only = TRUE)
+options(enrichplot.colours = c("red","blue"))
+
+# Define the symbols of interest
+symbol_genes <- c("CDH3", "ERBB2", "TYMS", "GREB1", "OSR1", 
+                  "MYBL2", "FAM83D", "ESR1", "FOXC1", "NAT1")
+gene_info <- fread("Step1_Data_collection_preprocessing/Results/peak.annotation.csv")
+
+# Subset gene_info to match SYMBOL values and create a named vector
+names(symbol_genes) <- gene_info[SYMBOL %in% symbol_genes, geneId]
+
+GO_BP <- enrichGO(gene = names(symbol_genes),
+                  OrgDb = org.Hs.eg.db,
+                  ont = "BP",
+                  pvalueCutoff = 1,
+                  qvalueCutoff = 0.1,
+                  readable = TRUE)
+
+df_GO_BP <- GO_BP@result %>%
+  as.data.frame() %>%
+  dplyr::select(-p.adjust)
+
+dotplot(GO_BP, x = "GeneRatio", showCategory = 10, 
+        color = "qvalue", font.size = 12, label_format = 100,
+        title = "GO: Biological process") + aes(shape = I(16)) + 
+  aes(color = qvalue) + 
+  set_enrichplot_color(type = "color", name = "qvalue")
+
+KEEG <- enrichKEGG(gene = names(symbol_genes),
+                   organism = 'hsa',
+                   pvalueCutoff = 1,
+                   qvalueCutoff = 0.1)
+
+dotplot(KEEG, x = "GeneRatio", showCategory = 10, 
+        color = "qvalue", font.size = 12, label_format = 100,
+        title = "KEGG pathway") + aes(shape = I(16)) + 
+  aes(color = qvalue) + 
+  set_enrichplot_color(type = "color", name = "qvalue")
